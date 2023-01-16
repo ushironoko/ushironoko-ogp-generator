75300cb391e67c2fad8b94ed67bd6cc717fb06ac
ushironoko <apple19940820@gmail.com>
2023-01-17 00:17:20 +0900

    add ogp api

---
+++ src/pages/api/og.tsx
@@ -0,0 +1,46 @@
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "experimental-edge",
+};
+
+export default function handler(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+
+    const hasTitle = searchParams.has("title");
+    const title = hasTitle
+      ? searchParams.get("title")?.slice(0, 100)
+      : "ushironoko.me";
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            fontSize: 60,
+            backgroundColor: "#fff",
+            backgroundSize: "100% 100%",
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {title}
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 600,
+      }
+    );
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
+}
